b9d93826d42d25aa6b680826b8114c8c4a75dcc4
Krishnendu Sekhar Das <krishnendu.sekhardas@indusnet.co.in>
2017-01-25 18:59:28 +0530

    fix iOS and mismatch key return for getImageDimension method

--- src/ios/KImageCropper.m
+++ src/ios/KImageCropper.m
@@ -78,7 +78,7 @@
             NSMutableDictionary *imgObj = [[NSMutableDictionary alloc] init];
             [imgObj setValue:[NSNumber numberWithInt:image.size.width] forKey:@"width"];
             [imgObj setValue:[NSNumber numberWithInt:image.size.height] forKey:@"height"];
-            [imgObj setValue:[NSString stringWithString:imagePath] forKey:@"imagePath"];
+            [imgObj setValue:[NSString stringWithString:imagePath] forKey:@"imgPath"];
             [finalImages addObject:imgObj];
         }
     }
