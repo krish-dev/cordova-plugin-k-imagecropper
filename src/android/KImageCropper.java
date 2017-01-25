b9036ce2751f260cd01e13a7645355d4a64aad69
Krishnendu Sekhar Das <krishnendu.sekhardas@indusnet.co.in>
2017-01-25 17:33:40 +0530

    fix zero width height  issue for media path android.

--- src/android/KImageCropper.java
+++ src/android/KImageCropper.java
@@ -1,9 +1,12 @@
 package in.co.indusnet.plugins.cordova.imagecropper;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.util.Log;
 
 import java.io.File;
@@ -84,9 +87,18 @@ public class KImageCropper extends CordovaPlugin {
             for(Integer i=0;i<imagePathsArr.length();i++) {
                 JSONObject img = new JSONObject();
                 String imgPath = imagePathsArr.getString(i);
+                String finalImgPath;
 
-                img = imageRatioCalculation(Uri.parse(imgPath));
+                // checking media path or absolute path.
+                String ext = imgPath.substring(imgPath.lastIndexOf(".") + 1);
+                if(!ext.equals("jpg") || !ext.equals("jpeg") || !ext.equals("JPG") || !ext.equals("JPEG") || !ext.equals("png") || !ext.equals("PNG")) {
+                    finalImgPath = getRealPathFromURI(cordova.getActivity().getApplicationContext(),Uri.parse(imgPath));
+                }else {
+                    finalImgPath =  imgPath;
+                }
+                img = imageRatioCalculation(Uri.parse(finalImgPath));
                 img.put("imgPath", imgPath);
+                img.put("imgPathAbsolute", finalImgPath);
                 successArr.put(img);
             }
 
@@ -96,6 +108,21 @@ public class KImageCropper extends CordovaPlugin {
             cordovaCallbackContext.error(e.getMessage());
         }
 
+    }
+
+    public String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
     }
 
     private void openCropper() {
